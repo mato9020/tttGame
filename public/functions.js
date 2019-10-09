a4813833cad77c277de343e58ffd02f6b014f833
= <matteson@udel.edu>
2019-10-09 09:41:57 -0400

    updated functionjs to have necessary config

---
+++ public/functions.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyC-dt6CIq0V2BRl9hTvJBjUk5kQRrmOUIk",
+    authDomain: "tttserver-b12d8.firebaseapp.com",
+    databaseURL: "https://tttserver-b12d8.firebaseio.com",
+    projectId: "tttserver-b12d8",
+    storageBucket: "tttserver-b12d8.appspot.com",
+    messagingSenderId: "10829566950",
+    appId: "1:10829566950:web:f43395bca099241b927c27",
+    measurementId: "G-F1PBSWNL82"
+  };
+  // Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
